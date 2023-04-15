ca5f5c5c6d6337e30c38ddc68e73c79ad68c1968
Viktoriya Zaporozhan <viktoriya.zaporozhan@nixsolutions.com>
2023-04-15 18:51:07 +0300

    test husky

--- src/components/LoginButtons.tsx
+++ src/components/LoginButtons.tsx
@@ -9,7 +9,6 @@ interface Props {
 }
 
 export const LoginButtons: FC<Props> = ({ googleLoginHandler, appleLoginHandler}) => {
-  console.log('blabla');
   return (
     <div className="login-buttons">
       <Button
